e621b0f04ccb369e6e69f52fbf4446f54d570176
bugatro <87689841+bugatro@users.noreply.github.com>
2023-07-26 16:04:45 +0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_InfinixX653C.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_InfinixX653C-user \
+    omni_InfinixX653C-userdebug \
+    omni_InfinixX653C-eng
