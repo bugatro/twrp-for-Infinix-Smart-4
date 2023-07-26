b47fa9c0d7193426ac5672dc25be4c23819c2089
bugatro <87689841+bugatro@users.noreply.github.com>
2023-07-26 20:34:15 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/InfinixX653C
+DEVICE_PATH := device/infinix/Infinix-X653C
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
